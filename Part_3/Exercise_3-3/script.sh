45086ee3c9159903a2b5edd3b64a0e04efdfdd69
Valerio Tatananni <valerio.tatananni@gmail.com>
2024-06-14 18:08:20 +0300

    Add Exercise 3.3

---
+++ Part_3/Exercise_3-3/script.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <github_repo> <dockerhub_repo>"
+  exit 1
+fi
+
+GITHUB_REPO=$1
+DOCKERHUB_REPO=$2
+GITHUB_REPO_URL="https://github.com/$GITHUB_REPO.git"
+IMAGE_TAG="latest"
+
+git clone $GITHUB_REPO_URL
+if [ $? -ne 0 ]; then
+  echo "Failed to clone the GitHub repository."
+  exit 1
+fi
+
+REPO_NAME=$(basename $GITHUB_REPO .git)
+
+cd $REPO_NAME
+
+docker build -t $DOCKERHUB_REPO:$IMAGE_TAG .
+if [ $? -ne 0 ]; then
+  echo "Failed to build the Docker image."
+  exit 1
+fi
+
+docker push $DOCKERHUB_REPO:$IMAGE_TAG
+if [ $? -ne 0 ]; then
+  echo "Failed to push the Docker image to DockerHub."
+  exit 1
+fi
+
+echo "Docker image pushed to DockerHub successfully."
+
